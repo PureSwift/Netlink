5820c4c7f5466327f17de72e54f6d7fa7fa65770
Alsey Coleman Miller <alseycmiller@gmail.com>
2021-10-10 20:26:23 -0500

    Added `NetlinkSocketAddress`

---
+++ Sources/Netlink/SocketAddress.swift
@@ -0,0 +1,52 @@
+//
+//  SocketAddress.swift
+//  
+//
+//  Created by Alsey Coleman Miller on 10/10/21.
+//
+
+import SystemPackage
+import CNetlink
+
+/// Netlink Socket Address
+@frozen
+public struct NetlinkSocketAddress: SocketAddress, Equatable, Hashable {
+    
+    /// Socket Address Family
+    @_alwaysEmitIntoClient
+    public static var family: SocketAddressFamily { return .netlink }
+    
+    /// Port ID
+    public var portID: UInt32
+    
+    /// Multicast groups mask.
+    public var groups: UInt32
+    
+    @_alwaysEmitIntoClient
+    public init(portID: UInt32 = numericCast(ProcessID.current.rawValue),
+                groups: UInt32 = 0) {
+        
+        self.portID = portID
+        self.groups = groups
+    }
+    
+    /// Unsafe pointer closure
+    public func withUnsafePointer<Result>(
+      _ body: (UnsafePointer<CInterop.SocketAddress>, UInt32) throws -> Result
+    ) rethrows -> Result {
+        let socketAddress = sockaddr_nl(
+            nl_family: .init(Self.family.rawValue),
+            nl_pad: 0,
+            nl_pid: portID,
+            nl_groups: groups
+        )
+        return try socketAddress.withUnsafePointer(body)
+    }
+}
+
+extension sockaddr_nl: CSocketAddress {
+    
+    /// Socket Address Family
+    @_alwaysEmitIntoClient
+    public static var family: SocketAddressFamily { return .netlink }
+}
