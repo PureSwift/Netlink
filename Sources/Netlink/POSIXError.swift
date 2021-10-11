9fd14cce6dfebf55ae1153c22bd5d44f597db571
Alsey Coleman Miller <alseycmiller@gmail.com>
2021-10-10 20:26:58 -0500

    Updated `POSIXError`

--- Sources/Netlink/POSIXError.swift
+++ Sources/Netlink/POSIXError.swift
@@ -1,35 +1,25 @@
 //
 //  POSIXError.swift
-//  SwiftFoundation
+//  
 //
 //  Created by Alsey Coleman Miller on 7/22/15.
-//  Copyright © 2015 PureSwift. All rights reserved.
 //
 
 import Foundation
 
-public extension POSIXError {
-    
-    /// Creates error from C ```errno```.
-    static var fromErrno: POSIXError? {
-        
-        guard let code = POSIXErrorCode(rawValue: errno)
-            else { return nil }
-        
-        return self.init(code: code)
-    }
-    
-    #if os(macOS) || os(iOS) || os(watchOS) || os(tvOS)
+#if canImport(Darwin)
+internal extension POSIXError {
     
     /// Creates `POSIXError` from error code.
     init(code: POSIXErrorCode) {
         
-        let nsError = NSError(domain: NSPOSIXErrorDomain,
-                              code: Int(code.rawValue),
-                              userInfo: nil)
+        let nsError = NSError(
+            domain: NSPOSIXErrorDomain,
+            code: Int(code.rawValue),
+            userInfo: [:]
+        )
         
         self.init(_nsError: nsError)
     }
-    
-    #endif
 }
+#endif
